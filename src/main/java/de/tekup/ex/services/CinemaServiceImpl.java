5b460d3cc424a7007641378dee2dd01c015d358f
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2020-11-30 14:28:42 +0100

    Service 1

--- src/main/java/de/tekup/ex/services/CinemaServiceImpl.java
+++ src/main/java/de/tekup/ex/services/CinemaServiceImpl.java
@@ -1,10 +1,16 @@
 package de.tekup.ex.services;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import de.tekup.ex.models.Movie;
+import de.tekup.ex.models.Star;
 import de.tekup.ex.models.Studio;
 import de.tekup.ex.repositories.MovieRepository;
 import de.tekup.ex.repositories.StarRepository;
@@ -21,8 +27,20 @@ public class CinemaServiceImpl implements CinemaService {
 
 	@Override
 	public List<Studio> getStudiosByStar(String starName) {
-		// TODO Auto-generated method stub
-		return null;
+		Star star = reposStar.findById(starName)
+				.orElseThrow(()-> new NoSuchElementException("Star with this name is not found"));
+		Set<Studio> studios = new HashSet<>();
+		
+		for (Movie movie : star.getMovies()) {
+			studios.add(movie.getStudio());
+			
+		}
+		return new ArrayList<>(studios);
+		/*return star.getMovies().stream()
+								.map(movie -> movie.getStudio())
+								.distinct()
+								.collect(Collectors.toList());
+								*/
 	}
 
 	@Override
